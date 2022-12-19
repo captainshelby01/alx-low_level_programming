103b8946be07febfd5408f46fe1fe834efc13337
captainshelby01 <flamxypii@gmail.com>
2022-12-19 14:36:09 -0800

    task 5 fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -17,9 +17,9 @@ void rev_string(char *s)
 
 	length2 = length1 - 1
 	for (i = 0; i < length1 / 2; i++)
-	i{
+	{
 		tmp = s[i];
 		s[i] = s[length2];
 		s[length2] = tmp;
-		length2 = 1;
+		length2 -= 1;
 }
